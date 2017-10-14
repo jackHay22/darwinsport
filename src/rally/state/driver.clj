29396ba633510c7a6dcf851aeabef4994e2f3d5e
jackHay22 <jjphay@gmail.com>
2017-10-14 19:28:38 -0400

    updates to key event system

---
+++ src/rally/state/driver.clj
@@ -0,0 +1,6 @@
+(ns rally.state.driver
+  (:gen-class)
+)
+
+(defn take-input
+  [code] (println code))
--- src/rally/window/mainwindow.clj
+++ src/rally/window/mainwindow.clj
@@ -1,9 +1,11 @@
 (ns rally.window.mainwindow
+  (:require [rally.state.driver :as state])
   (:gen-class))
 
 ;Java import
 (import '(java.awt Color Dimension Graphics Point))
-(import '(javax.swing JPanel JFrame JLabel))
+(import '(java.awt.event KeyListener KeyEvent))
+(import '(javax.swing JPanel JFrame SwingUtilities))
 
 ;graphical setup
 (def frame-width 1000)
@@ -21,20 +23,22 @@
 (defn init-window
   "get jpanel in jframe, setup prefs"
   []
+  (let [event-driver (proxy [java.awt.event.KeyListener] []
+                 (keyPressed [^KeyEvent e] (if (= (.getKeyChar e) \q)
+                          (System/exit 0)
+                          (state/take-input (.getKeyChar e))))
+                 (keyReleased [e])
+                 (keyTyped [e]))]
   (doto frame
     (.setSize frame-width frame-height)   ;set frame size from preset
     (.setResizable false)
     (.add panel)
     (.pack)
-    ;(.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE) ; (repl problem)
+    (.addKeyListener event-driver)
+    (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE) ; (repl problem)
     (.setVisible true)
   )
-)
+))
 
-(defn start-window
-  "initialize plotter window and build graphical elements"
-  []
-  (init-window)       ;build up window
-  (Thread/sleep 500)  ;needs a slight delay (can dial this back for optimization)
-  "Window created..."
-)
+(defn start-window []
+  (SwingUtilities/invokeLater #(init-window)))
