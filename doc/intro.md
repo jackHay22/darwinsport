5ad2fcecec70611f4ac117387c224b6740a5e9f2
Jack Hay <jjphay@gmail.com>
2017-10-13 23:33:36 -0400

    Add files via upload

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rally
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/rally/core_test.clj
@@ -0,0 +1,7 @@
+(ns rally.core-test
+  (:require [clojure.test :refer :all]
+            [rally.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
