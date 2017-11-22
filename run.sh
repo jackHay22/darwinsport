a3e52db9bc4dc9d527c6a2aa6cf2be972c376cd8
jackHay22 <jjphay@gmail.com>
2017-11-21 21:50:30 -0500

    add unicode racecar

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 RED='\033[0;31m'
+YELLOW='\033[1;33m'
 NC='\033[0m'
-printf "Starting ${RED}RallyEvolve${NC} node ${1} \n"
+CAR='\xF0\x9F\x8F\x8E'
+printf "${CAR}  Starting ${RED}RallyEvolve${NC} node ${YELLOW}${1}${NC} \n"
 exec java -jar /app.jar $1
