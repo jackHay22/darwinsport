fa779ed455ea49d5d6a9ea71f5090ad2062d6a8f
jackHay22 <jjphay@gmail.com>
2017-11-21 21:25:43 -0500

    updates

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ MAINTAINER Jack Hay
 ADD target/uberjar/rally-0.1.0-SNAPSHOT-standalone.jar app.jar
 ADD run.sh /run.sh
 
+ENV INSTANCE=$INSTANCE
+
 EXPOSE 5555
 
 ENTRYPOINT ./run.sh $INSTANCE
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 RED='\033[0;31m'
 NC='\033[0m'
-printf "Starting ${RED}RallyEvolve${NC} node \n"
-exec java -jar /app.jar
+printf "Starting ${RED}RallyEvolve${NC} node ${1} \n"
+exec java -jar /app.jar $1
